9ad2d848c073d378f52651df535099e46e5b8761
Fredmark Ivan D. Dizon <114529635+veilwr4ith@users.noreply.github.com>
2024-08-22 10:05:57 +0800

    Create lala.py

---
+++ lala.py
@@ -0,0 +1,78 @@
+from random import randint
+import sys
+
+
+def generator(g, x, p):
+    return pow(g, x) % p
+
+
+def encrypt(plaintext, key):
+    cipher = []
+    for char in plaintext:
+        cipher.append(((ord(char) * key*311)))
+    return cipher
+
+def decrypt(ciphertext, key):
+    plain = ""
+    for num in ciphertext:
+        plain += chr(int((num / key / 311)))
+    return plain
+
+def is_prime(p):
+    v = 0
+    for i in range(2, p + 1):
+        if p % i == 0:
+            v = v + 1
+    if v > 1:
+        return False
+    else:
+        return True
+
+
+def dynamic_xor_encrypt(plaintext, text_key):
+    cipher_text = ""
+    key_length = len(text_key)
+    for i, char in enumerate(plaintext[::-1]):
+        key_char = text_key[i % key_length]
+        encrypted_char = chr(ord(char) ^ ord(key_char))
+        cipher_text += encrypted_char
+    return cipher_text
+
+def dynamic_xor_decrypt(ciphertext, text_key):
+    plain_text = ""
+    key_length = len(text_key)
+    for i, char in enumerate(ciphertext):
+        key_char = text_key[i % key_length]
+        decrypted_char = chr(ord(char) ^ ord(key_char))
+        plain_text += decrypted_char
+    return plain_text
+
+
+def test(cipher_text, text_key):
+    p = 97
+    g = 31
+    if not is_prime(p) and not is_prime(g):
+        print("Enter prime numbers")
+        return
+    a = 97 #randint(p-10, p)
+    b = 22 #randint(g-10, g)
+    print(f"a = {a}")
+    print(f"b = {b}")
+    u = generator(g, a, p)
+    v = generator(g, b, p)
+    key = generator(v, a, p)
+    b_key = generator(u, b, p)
+    shared_key = None
+    if key == b_key:
+        shared_key = key
+    else:
+        print("Invalid key")
+        return
+    semi_cipher = decrypt(cipher_text, shared_key)
+    plaintext = dynamic_xor_decrypt(semi_cipher, text_key)
+    print(f'Plaintext: {plaintext[::-1]}')
+
+
+if __name__ == "__main__":
+    message = [151146, 1158786, 1276344, 1360314, 1427490, 1377108, 1074816, 1074816, 386262, 705348, 0, 1393902, 352674, 83970, 1141992, 0, 369468, 1444284, 16794, 1041228, 403056, 453438, 100764, 100764, 285498, 100764, 436644, 856494, 537408, 822906, 436644, 117558, 201528, 285498]
+    test(message, "trudeau")
